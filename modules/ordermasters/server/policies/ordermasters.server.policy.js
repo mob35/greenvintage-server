77334ab6013fc0070629ebf3f8a1f543c598f72d
Bu4y <amnes1a@windowslive.com>
2017-09-07 19:12:37 +0700

    update

--- modules/ordermasters/server/policies/ordermasters.server.policy.js
+++ modules/ordermasters/server/policies/ordermasters.server.policy.js
@@ -28,7 +28,7 @@ exports.invokeRolesPolicies = function () {
       permissions: ['get', 'post']
     }, {
       resources: '/api/ordermasters/:ordermasterId',
-      permissions: ['get']
+      permissions: '*'
     }]
   }, {
     roles: ['guest'],
