8f048405374f337e99076a17b50c4e36a75919cd
tingtang35 <tingtang35@gmail.com>
2017-09-11 17:37:57 +0700

    update product completed

--- modules/products/tests/server/product.server.model.tests.js
+++ modules/products/tests/server/product.server.model.tests.js
@@ -17,8 +17,8 @@ var user,
 /**
  * Unit tests
  */
-describe('Product Model Unit Tests:', function() {
-  beforeEach(function(done) {
+describe('Product Model Unit Tests:', function () {
+  beforeEach(function (done) {
     user = new User({
       firstName: 'Full',
       lastName: 'Name',
@@ -28,9 +28,15 @@ describe('Product Model Unit Tests:', function() {
       password: 'password'
     });
 
-    user.save(function() {
+    user.save(function () {
       product = new Product({
         name: 'Product Name',
+        detail: 'Product Detail',
+        price: 100,
+        promotionprice: 80,
+        percentofdiscount: 20,
+        currency: '฿',
+        images: ['https://store.storeimages.cdn-apple.com/8750/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/black/iphone7-black-select-2016?wid=300&hei=300&fmt=png-alpha&qlt=95&.v=1472430037379', 'https://store.storeimages.cdn-apple.com/8750/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/rosegold/iphone7-rosegold-select-2016?wid=300&hei=300&fmt=png-alpha&qlt=95&.v=1472430205982'],
         user: user
       });
 
@@ -38,28 +44,47 @@ describe('Product Model Unit Tests:', function() {
     });
   });
 
-  describe('Method Save', function() {
-    it('should be able to save without problems', function(done) {
+  describe('Method Save', function () {
+    it('should be able to save without problems', function (done) {
       this.timeout(0);
-      return product.save(function(err) {
+      return product.save(function (err) {
         should.not.exist(err);
         done();
       });
     });
 
-    it('should be able to show an error when try to save without name', function(done) {
+    it('should be able to show an error when try to save without name', function (done) {
       product.name = '';
 
-      return product.save(function(err) {
+      return product.save(function (err) {
         should.exist(err);
         done();
       });
     });
+
+    it('should be able to show an error when try to save without price', function (done) {
+      product.price = null;
+
+      return product.save(function (err) {
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('should be able to show an error when try to save without images', function (done) {
+      product.images = [];
+
+      return product.save(function (err) {
+        should.exist(err);
+        done();
+      });
+    });
+
   });
 
-  afterEach(function(done) {
-    Product.remove().exec(function() {
-      User.remove().exec(function() {
+  afterEach(function (done) {
+    Product.remove().exec(function () {
+      User.remove().exec(function () {
         done();
       });
     });
