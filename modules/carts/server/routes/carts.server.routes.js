daba806403db338dc7a8554727e45086fb4c5c72
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-09-12 17:34:10 +0700

    update cart

--- modules/carts/server/routes/carts.server.routes.js
+++ modules/carts/server/routes/carts.server.routes.js
@@ -8,16 +8,16 @@ var cartsPolicy = require('../policies/carts.server.policy'),
 
 module.exports = function (app) {
   // Carts Routes
-  app.route('/api/carts').all(cartsPolicy.isAllowed)
+  app.route('/api/carts')//.all(cartsPolicy.isAllowed)
     .get(carts.list)
     .post(carts.cartID, carts.update, carts.create, carts.readCart);
 
-  app.route('/api/carts/:cartId').all(cartsPolicy.isAllowed)
+  app.route('/api/carts/:cartId')//.all(cartsPolicy.isAllowed)
     .get(carts.read)
     .put(carts.update)
     .delete(carts.delete);
 
-  app.route('/api/cart/user/:userId').all(cartsPolicy.isAllowed)
+  app.route('/api/cart/user/:userId')//.all(cartsPolicy.isAllowed)
     .get(carts.readCart);
 
   // Finish by binding the Cart middleware
